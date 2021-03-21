c387e3915dec421a805e194d6091f728148a7685
Yusuf Çağlar <faf7faf7@gmail.com>
2021-03-22 00:12:33 +0300

    Initial Commit

---
+++ WhatsAppClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsAppClone
+//
+//  Created by Yusuf ÇAĞLAR on 22/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Yusuf ÇAĞLAR on 22/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
